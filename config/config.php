4f0ddcc4f9140d773b43e21200ce3fb0444695bf
Nejc Krajnik <nejc.krajnik@zabec.net>
2015-11-29 15:12:20 +0100

    git ignore fix, configuration file added

---
+++ config/config.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+     /*
+     |--------------------------------------------------------------------------
+     | API username for hlrlookups.com
+     |--------------------------------------------------------------------------
+     |
+     |  Username for hlrlookups.com
+     |
+     */
+
+     'API_USERNAME' => 'hlrlookups_com_username',
+
+     /*
+     |--------------------------------------------------------------------------
+     | API password for hlrlookups.com
+     |--------------------------------------------------------------------------
+     |
+     | Password for hlrlookups.com
+     |
+     */
+     'API_PASS' => 'hlrlookups_com_password',
+
+     /*
+     |--------------------------------------------------------------------------
+     | Our Application Auth API key
+     |--------------------------------------------------------------------------
+     |
+     | Our Application api key
+     |
+     */
+     'API_KEY' => 'your_awesome_api_key_for_the_app'
+ ];
